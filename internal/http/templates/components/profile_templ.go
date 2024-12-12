b3d99d68560338520252743d7075e6328480f14e
naliah <naliah.web@gmail.com>
2024-12-12 20:27:49 +0000

    i complete profile changing

--- internal/http/templates/components/profile_templ.go
+++ internal/http/templates/components/profile_templ.go
@@ -9,6 +9,7 @@ import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
 import "github.com/vixdang0x7d3/the-human-task-manager/internal/http/models"
+import "github.com/vixdang0x7d3/the-human-task-manager/internal/http/templates/pages"
 
 func ChangeInfoForm(m models.UserView, postURL string) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
@@ -31,7 +32,7 @@ func ChangeInfoForm(m models.UserView, postURL string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"user-info\" class=\"w-full text-left py-2\"><form hx-post=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<form hx-post=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -44,7 +45,7 @@ func ChangeInfoForm(m models.UserView, postURL string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" hx-target=\"#user-info\" hx-swap=\"outerHTML\"><label class=\"form-control w-full max-w-xs\"><div class=\"label\"><span class=\"label-text\">Username</span></div><label id=\"username-label\" class=\"input input-sm w-full input-bordered flex items-center gap-2\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\" fill=\"currentColor\" class=\"h-4 w-4 opacity-70\"><path d=\"M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z\"></path></svg> <input id=\"username\" name=\"username\" type=\"text\" class=\"grow\" value=\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" hx-target=\"#user-info\" hx-target-error=\"#message\"><label class=\"form-control w-full\"><div class=\"label\"><span class=\"label-text\">Username</span></div><label id=\"username-label\" class=\"input w-full input-sm input-bordered flex items-center gap-2\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\" fill=\"currentColor\" class=\"h-4 w-4 opacity-70\"><path d=\"M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z\"></path></svg> <input id=\"username\" name=\"username\" type=\"text\" class=\"grow\" value=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -83,7 +84,215 @@ func ChangeInfoForm(m models.UserView, postURL string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"></label></div><div class=\"card-actions justify-end\"><button type=\"submit\" id=\"save-info\" class=\"btn btn-sm btn-neutral mt-4\">Save</button></div></form></div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"></label></div><div id=\"message\" class=\"text-error text-center\"></div><div class=\"card-actions justify-end\"><button type=\"submit\" id=\"save-info\" class=\"btn btn-sm btn-neutral mt-4\">Save</button></div></form>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		return templ_7745c5c3_Err
+	})
+}
+
+func SavedInfoForm(m models.UserView, postURL string) templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
+			return templ_7745c5c3_CtxErr
+		}
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var6 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var6 == nil {
+			templ_7745c5c3_Var6 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"user-info\" class=\"w-full text-left py-2\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = pages.InfoForm(m, postURL).Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		return templ_7745c5c3_Err
+	})
+}
+
+func ChangeEmailForm(m models.UserView, postURL string) templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
+			return templ_7745c5c3_CtxErr
+		}
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var7 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var7 == nil {
+			templ_7745c5c3_Var7 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<form hx-post=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var8 string
+		templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(string(templ.URL(postURL)))
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/profile.templ`, Line: 69, Col: 43}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" hx-target=\"#user-email\" hx-target-error=\"#message\"><div class=\"flex flex-row justify-between\"><label class=\"form-control w-full max-w-xs\"><div class=\"label\"><span class=\"label-text\">Email</span></div><label class=\"input input-bordered input-sm flex items-center gap-2\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\" fill=\"currentColor\" class=\"h-4 w-4 opacity-70\"><path d=\"M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z\"></path> <path d=\"M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z\"></path></svg> <input id=\"email\" name=\"email\" type=\"text\" class=\"grow\" value=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var9 string
+		templ_7745c5c3_Var9, templ_7745c5c3_Err = templ.JoinStringErrs(m.Email)
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/profile.templ`, Line: 85, Col: 76}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var9))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"></label></label> <label class=\"form-control\"><div class=\"label\"><div id=\"message\" class=\"text-error text-center\"></div></div><button id=\"change-email\" class=\"btn btn-sm btn-neutral mt-4 ml-2\">Save Email</button></label></div></form>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		return templ_7745c5c3_Err
+	})
+}
+
+func SavedEmailForm(m models.UserView, postURL string) templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
+			return templ_7745c5c3_CtxErr
+		}
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var10 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var10 == nil {
+			templ_7745c5c3_Var10 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"user-email\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = pages.EmailForm(m, postURL).Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		return templ_7745c5c3_Err
+	})
+}
+
+func ChangePasswordForm(postURL string) templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
+			return templ_7745c5c3_CtxErr
+		}
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var11 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var11 == nil {
+			templ_7745c5c3_Var11 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<form hx-post=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var12 string
+		templ_7745c5c3_Var12, templ_7745c5c3_Err = templ.JoinStringErrs(string(templ.URL(postURL)))
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/profile.templ`, Line: 105, Col: 43}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var12))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" hx-target=\"#user-password\" hx-target-error=\"#message\"><div class=\"flex flex-row justify-between\"><div><label class=\"form-control w-full max-w-xs\"><div class=\"label\"><span class=\"label-text\">Current Password</span></div><label class=\"input input-bordered input-sm flex items-center gap-2\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\" fill=\"currentColor\" class=\"h-4 w-4 opacity-70\"><path fill-rule=\"evenodd\" d=\"M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z\" clip-rule=\"evenodd\"></path></svg> <input id=\"current-password\" name=\"current-password\" type=\"password\" class=\"grow\"></label></label> <label class=\"form-control w-full max-w-xs\"><div class=\"label\"><span class=\"label-text\">New Password</span></div><label class=\"input input-bordered input-sm flex items-center gap-2\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\" fill=\"currentColor\" class=\"h-4 w-4 opacity-70\"><path fill-rule=\"evenodd\" d=\"M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z\" clip-rule=\"evenodd\"></path></svg> <input id=\"new-password\" name=\"new-password\" type=\"password\" class=\"grow\"></label></label></div><label class=\"form-control\"><div class=\"label\"><div id=\"message\" class=\"text-error text-center\"></div></div><button id=\"change-password\" class=\"btn btn-sm btn-neutral mt-4 ml-2\">Save Password</button></label></div></form>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		return templ_7745c5c3_Err
+	})
+}
+
+func SavedPasswordForm(postURL string) templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
+			return templ_7745c5c3_CtxErr
+		}
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var13 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var13 == nil {
+			templ_7745c5c3_Var13 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"user-password\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = pages.PasswordForm(postURL).Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- internal/http/user.go
+++ internal/http/user.go
@@ -16,6 +16,11 @@ func (s *Server) registerUserRoutes(r *echo.Group) {
 	r.GET("/profile", s.handleProfileShow)
 
 	r.POST("/change-info", s.handleChangeProfileInfo)
+	r.POST("/change-info-save", s.handleSaveProfileInfoChange)
+	r.POST("/change-email", s.handleChangeProfileEmail)
+	r.POST("/change-email-save", s.handleSaveProfileEmailChange)
+	r.POST("/change-password", s.handleChangeProfilePassword)
+	r.POST("/change-password-save", s.handleSaveprofilePasswordChange)
 
 	r.DELETE("/logout", s.handleLogout)
 }
@@ -46,7 +51,7 @@ func (s *Server) handleProfileShow(c echo.Context) error {
 		Email:     user.Email,
 	}
 
-	return render(c, http.StatusOK, pages.Profile(m, "/change-info", "/logout"))
+	return render(c, http.StatusOK, pages.Profile(m, "/change-info", "/change-email", "/change-password", "/logout"))
 }
 
 func (s *Server) handleChangeProfileInfo(c echo.Context) error {
@@ -75,6 +80,111 @@ func (s *Server) handleChangeProfileInfo(c echo.Context) error {
 	}
 
 	return render(c, http.StatusOK, components.ChangeInfoForm(m, "/change-info-save"))
+}
+
+func (s *Server) handleSaveProfileInfoChange(c echo.Context) error {
+	type formValues struct {
+		Username  string `form:"username" validate:"required"`
+		FirstName string `form:"first_name" validate:"required"`
+		LastName  string `form:"last_name" validate:"required"`
+	}
+
+	form := formValues{}
+	if err := c.Bind(&form); err != nil {
+		c.Logger().Error(err)
+		return c.HTML(http.StatusBadRequest, "invalid form data")
+	}
+
+	if err := c.Validate(form); err != nil {
+		c.Logger().Error(err)
+		return c.HTML(http.StatusBadRequest, "invalid value")
+	}
+
+	// save info into database_______________________!!!
+
+	m := models.UserView{
+		Username:  form.Username,
+		FirstName: form.FirstName,
+		LastName:  form.LastName,
+	}
+
+	return render(c, http.StatusOK, components.SavedInfoForm(m, "change-info"))
+}
+
+func (s *Server) handleChangeProfileEmail(c echo.Context) error {
+	type formValues struct {
+		Email string `form:"email" validate:"required,email"`
+	}
+
+	form := formValues{}
+	if err := c.Bind(&form); err != nil {
+		c.Logger().Error(err)
+		return c.HTML(http.StatusBadRequest, "invalid form data")
+	}
+
+	if err := c.Validate(form); err != nil {
+		c.Logger().Error(err)
+		return c.HTML(http.StatusBadRequest, "invalid value")
+	}
+
+	m := models.UserView{
+		Email: form.Email,
+	}
+
+	return render(c, http.StatusOK, components.ChangeEmailForm(m, "/change-email-save"))
+}
+
+func (s *Server) handleSaveProfileEmailChange(c echo.Context) error {
+	type formValues struct {
+		Email string `form:"email" validate:"required,email"`
+	}
+
+	form := formValues{}
+	if err := c.Bind(&form); err != nil {
+		c.Logger().Error(err)
+		return render(c, http.StatusBadRequest, components.AlertError("invalid form data"))
+	}
+
+	if err := c.Validate(form); err != nil {
+		c.Logger().Error("form validation error", err)
+		return c.HTML(http.StatusBadRequest, "invalid email")
+	}
+
+	// save info into database_______________________!!!
+
+	m := models.UserView{
+		Email: form.Email,
+	}
+
+	return render(c, http.StatusOK, components.SavedEmailForm(m, "change-email"))
+}
+
+func (s *Server) handleChangeProfilePassword(c echo.Context) error {
+	return render(c, http.StatusOK, components.ChangePasswordForm("/change-password-save"))
+}
+
+func (s *Server) handleSaveprofilePasswordChange(c echo.Context) error {
+	type formValues struct {
+		CurrentPassword string `form:"current-password" validate:"required"`
+		NewPassword     string `form:"new-password" validate:"required"`
+	}
+
+	form := formValues{}
+	if err := c.Bind(&form); err != nil {
+		c.Logger().Error(err)
+		return c.HTML(http.StatusBadRequest, "invalid form data")
+	}
+
+	if err := c.Validate(form); err != nil {
+		c.Logger().Error(err)
+		return c.HTML(http.StatusBadRequest, "invalid password")
+	}
+
+	// check current password_______________________!!!
+
+	// save info into database_______________________!!!
+
+	return render(c, http.StatusOK, components.SavedPasswordForm("/change-password"))
 }
 
 // TODO: flash message support
